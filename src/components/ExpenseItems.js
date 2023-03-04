1186bddc9d2af695f61c705ab72e4767a9540981
krgarav <krgarav@gmail.com>
2023-03-04 18:21:03 +0530

    First commit

---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,12 @@
+
+
+function ExpenseItems(){
+    return (<div>
+        <h2>Expense Items</h2>
+        <h2>Food Rs10</h2>
+        <h2>Petrol Rs100</h2>
+        <h2>Movies Rs200</h2>
+    </div>);
+}
+
+export default ExpenseItems;
