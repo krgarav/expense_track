ec380a27a2ee0f0ed900151d6490cf2ba7603971
krgarav <krgarav@gmail.com>
2023-03-05 01:25:53 +0530

    Dynamic data entry using props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import ExpenseItems from "./components/ExpenseItems";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      location: "Dmart",
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49,location: "Ezone", date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      location: "Bajaj",
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      location: "Ikea",
+      date: new Date(2021, 5, 12),
+    },
+  ];
+  const list = [];
+  for (let i = 0; i < 4; i++) {
+    list.push(
+      <ExpenseItems
+        title={expenses[i].title}
+        amount={expenses[i].amount}
+        location={expenses[i].location}
+        date={expenses[i].date}
+      ></ExpenseItems>
+    );
+  }
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItems></ExpenseItems>
+      {list}
     </div>
   );
 }
--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,12 +1,16 @@
+import "./ExpenseItems.css";
 
+function ExpenseItems(props){
 
-function ExpenseItems(){
-    return (<div>
-        <h2>Expense Items</h2>
-        <h2>Food Rs10</h2>
-        <h2>Petrol Rs100</h2>
-        <h2>Movies Rs200</h2>
-    </div>);
+    return (<div className="expense-item">
+    <div>{props.date.toDateString()}</div>
+    <div className="expense-item__description">
+    <h2>{props.title}</h2>
+    <h2>: {props.location}</h2>
+    <div className="expense-item__price">Rs {props.amount}/-</div>
+    </div>
+    
+</div>);
 }
 
 export default ExpenseItems;
