a6ac872c6b04bd73b96e5232815793545a833a42
krgarav <krgarav@gmail.com>
2023-03-12 22:58:15 +0530

    Added deleteItems

--- src/components/Expenses/ExpenseItems.js
+++ src/components/Expenses/ExpenseItems.js
@@ -4,13 +4,17 @@ import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from "../UI/Card";
 
-const ExpenseItems=(props)=>{
+const ExpenseItems = (props) => {
+  const deleteHandler = (event) => {
+    event.target.parentNode.style.display = "none";
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <ExpenseDetails amount={props.amount} title={props.title} />
+      <button onClick={deleteHandler}>Delete Item</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItems;
